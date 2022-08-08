12c0cc887a8caa6c78df0f078c3b7603cae42905
WojciechWirowski <71640545+WojciechWirowski@users.noreply.github.com>
2022-08-08 10:06:16 +0200

    Front end broken by router. Since now working: -support communication, -shop, -basket, -navigation bar

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  purge: ['./index.html', './src/**/*.{js,jsx,ts,tsx,vue}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
